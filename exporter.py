aabef28a48601319f9d7e369710f8fd1abd63f14
courageconnet <courage.kosana@connetcom.co.za>
2016-09-16 12:07:16 +0200

    graph reporting for each sheet

--- exporter.py
+++ exporter.py
@@ -1,42 +1,58 @@
 import pandas as pd
 import ijson
+class exporter(object):
+    def __init__(self):
+        self.dataframes = []
+    def excelExporter(self):
 
-dataframes=[]
-with open('request.log.2016-08-25.json') as f:
-    data=ijson.items(f,'item')
-    for i in data:
-        counter=1
-        idx=i['name']
-        trans=i['transactions']
-        dataf="df{0}".format(counter)
-        dataf=pd.DataFrame(trans,columns=[idx,'name','usage'])
+        with open('report.json') as f:
+            data=ijson.items(f,'item')
+            for i in data:
+                counter=1
+                idx=i['name']
+                trans=i['transactions']
 
-        dataf.index+=1
-
-        dataframes.append(dataf)
-
-        counter+=1
-        print(dataf)
-
-# Create a Pandas Excel writer using XlsxWriter as the engine.
-writer = pd.ExcelWriter('pandas_simple.xlsx', engine='xlsxwriter')
-
-for i in range(len(dataframes)):
+                dataf=pd.DataFrame(trans,columns=[idx,'type','usage'])
+                dataf.index+=1
 
 
-    # Convert the dataframe to an XlsxWriter Excel object.
-    sheetname = "sheet{0}".format(i)
+                self.dataframes.append(dataf)
 
-    dataframes[i].to_excel(writer, sheet_name=sheetname)
+                counter+=1
+                print(dataf)
+
+        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        writer = pd.ExcelWriter('report.xlsx', engine='xlsxwriter')
+        workbook = writer.book
+        for i in range(len(self.dataframes)):
+
+            counter+=1
+            # Convert the dataframe to an XlsxWriter Excel object.
+            sheetname = "sheet{0}".format(i)
+
+            self.dataframes[i].to_excel(writer, sheet_name=sheetname)
+
+            worksheet=writer.sheets[sheetname]
+            chart=workbook.add_chart({'type':'column'})
+            chart.add_series({'values':'={0}!$D$2:$D$12'.format(sheetname),
+                              'categories':[sheetname,1,2,13,2],
+                              'name':'={0}!$D1'.format(sheetname)})
+            worksheet.insert_chart('F3',chart)
+
+            chart.set_title({'name':'={0}!$B$1'.format(sheetname)})
+
+        writer.save()
 
 
-    # Close the Pandas Excel writer and output the Excel file.
-
-writer.save()
+            # Close the Pandas Excel writer and output the Excel file.
 
 
 
 
+
+if __name__=='__main__':
+    expe=exporter()
+    expe.excelExporter()
 
 
 
