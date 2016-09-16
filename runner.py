1dea0f2fd12e1eb85bc00f592702baa8fd7abb31
courageconnet <courage.kosana@connetcom.co.za>
2016-09-16 12:10:56 +0200

    adding the exporter to the run queue and  a profiler for timing

--- runner.py
+++ runner.py
@@ -1,11 +1,21 @@
 from util import Util
+from exporter import exporter
+import time
 class Runner:
 
     def run(self):
+        self.start=time.clock()
         ut=Util()
-        ut.fileCamel()
+        exp=exporter()
+        ut.fileconverter()
         ut.jsonFileReader()
-        ut.jsonFileReader()
+        exp.excelExporter()
+        self.profileit()
+    def profileit(self):
+        print(time.clock()-self.start)
+
+
+
 
 if __name__== "__main__":
     Runner().run()
