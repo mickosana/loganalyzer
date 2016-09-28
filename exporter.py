dc40116b3a7e1929682ebbce249526fbab18f2e7
courageconnet <courage.kosana@connetcom.co.za>
2016-09-28 16:02:48 +0200

    naming of report excel sheet according to date,creation of summary sheet,compination of all json objects to form one dataframe

--- exporter.py
+++ exporter.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import ijson
 class exporter(object):
-    def __init__(self):
+    def __init__(self,date):
         self.dataframes = []
         self.names=[]
-        self.date=''
+        self.date=date
         self.tempnames=[]
+        self.totalRequests=[]
+        self.indexes=[]
 
 
 
@@ -15,41 +17,55 @@ class exporter(object):
         with open('report.json') as f:
             data=ijson.items(f,'item')
             for i in data:
+                sum=0;
+                obj={}
                 counter=1
                 idx=i['name']
                 trans=i['transactions']
                 key=i['key']
-                self.date=i['date']
+                trans[0]['name']=idx
+                trans[0]['key']=key
+                for i in trans:
+                    sum=sum+i['usage']
                 self.names.append(idx)
-                dataf=pd.DataFrame(trans)
+                dataf=pd.DataFrame(trans,columns=['key','name','type','usage'])
                 dataf.index+=1
-                dataf.set_index(key,inplace=True)
-
-
                 self.dataframes.append(dataf)
+                obj['Total Requests'] = sum
+                obj['name']=idx
+                self.totalRequests.append(obj)
 
                 counter+=1
-                print(dataf)
 
-        # Create a Pandas Excel writer using XlsxWriter as the engine.
+        summaryDataFrame = pd.DataFrame(self.totalRequests,columns=['name','Total Requests'])
+        summaryDataFrame.set_index('name',inplace=True)
+
+        print(summaryDataFrame)
+        #Create a Pandas Excel writer using XlsxWriter as the engine.
         writer = pd.ExcelWriter('{0}.xlsx'.format(self.date) ,engine='xlsxwriter')
         workbook = writer.book
+        summaryDataFrame.to_excel(writer,sheet_name='Usage summary')
+        sheet=writer.sheets['Usage summary']
+        sheet.write('A18','Total Overaall Requests')
+        sheet.write_formula('B18','=SUM(B2:B17)')
         for i in range(len(self.dataframes)):
 
             counter+=1
-            # Conve rt the dataframe to an XlsxWriter Excel object.
+            #Convert the dataframe to an XlsxWriter Excel object.
 
 
             self.dataframes[i].to_excel(writer, sheet_name=self.names[i])
 
             worksheet=writer.sheets[self.names[i]]
             chart=workbook.add_chart({'type':'column'})
-            chart.add_series({'values':'={0}!$D$2:$D$12'.format(self.names[i]),
-                              'categories':[self.names[i],1,2,13,2],
+            chart.add_series({'values':'={0}!$E$2:$E$12'.format(self.names[i]),
+                              'categories':[self.names[i],1,3,13,3],
                               'name':'={0}!$D1'.format(self.names[i])})
             worksheet.insert_chart('F3',chart)
+            chart.set_title({'name':'={0}!$C$2'.format(self.names[i])})
+            worksheet.write('D15','Total Requests')
+            worksheet.write_formula('E15','=SUM(E2:E14)')
 
-            chart.set_title({'name':'={0}!$B$1'.format(self.names[i])})
 
         writer.save()
 
@@ -59,11 +75,6 @@ class exporter(object):
 
 
 
-
-
-if __name__=='__main__':
-    expe=exporter()
-    expe.excelExporter()
 
 
 
