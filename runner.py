0890844b763b21a75f48c3507b06266ce772c71b
DESKTOP-VQM44HG\Courage <ckprofmyk@gmail.com>
2016-11-23 12:04:41 +0200

    introduction of logging

--- runner.py
+++ runner.py
@@ -1,14 +1,27 @@
 from util import Util
 from exporter import exporter
 import time,json
-
+import logging
 class Runner:
     def __init__(self):
         self.logpath='',
         self.keypath=''
         self.date=''
+        self.logger = logging.getLogger("STATSREPORTER")
+        handler2=logging.StreamHandler()
+        handler2.setLevel(logging.INFO)
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s -%(levelname)s - %(message)s',
+                            datefmt='%y-%m-%d %H:%M',
+                            filename='statsreporter.log'
+                            )
+
+
+
     def run(self):
+        self.logger.log(logging.INFO,"##STARTING SESSION##")
         try:
+
             self.start=time.clock()
             self.configurator()
             ut=Util(self.keypath,self.date)
@@ -19,10 +32,11 @@ class Runner:
             exp.excelExporter()
             self.profileit()
 
-        except KeyboardInterrupt:
-            print("operation stopped by user")
+        except KeyboardInterrupt as e:
+            self.logger.error("oops program interrupted ..exiting",str(e))
         finally:
             ut.cleanup()
+            self.logger.log(logging.INFO,"##EXITING##")
 
 
 
@@ -35,9 +49,13 @@ class Runner:
     def configurator(self):
         with open('config.json','r') as configfile:
             config=json.load(configfile)
-            self.keypath=config['logpath']
-            self.logpath=config['keypath']
-            self.date=config['date']
+            try:
+
+                self.keypath=config['logpath']
+                self.logpath=config['keypath']
+                self.date=config['date']
+            except Exception as e:
+                self.logger.log(logging.ERROR,"an error occured while reading the config file",str(e))
 
 
 
@@ -56,4 +74,5 @@ class Runner:
 
 
 if __name__== "__main__":
+
     Runner().run()
